d4c7e4a47156fa5e8c46dfe35579bb98154627d4
rami-a <2364772+rami-a@users.noreply.github.com>
2020-04-21 09:36:07 -0400

    Add dev docs for FABs including dartpad demos (#35)

---
+++ docs/components/dartpad/fabs/regular/main.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: FloatingActionButton(
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+          onPressed: () {
+            // Respond to button press
+          },
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
+  }
+}
