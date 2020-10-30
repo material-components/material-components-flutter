5e7a8dc5d032af76f7cf0cb70b0a89642adf9202
Jose Alba Aguado <jaguado@google.com>
2020-10-30 16:58:14 -0400

    wrapped with scaffold and center

--- medium/material_icons/main.dart
+++ medium/material_icons/main.dart
@@ -15,28 +15,32 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: const [
-        Icon(
-          Icons.masks_rounded,
-          color: Colors.pink,
-          size: 24.0,
-          semanticLabel: 'Wear a mask',
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.masks_rounded,
+              color: Colors.pink,
+              size: 24.0,
+              semanticLabel: 'Wear a mask',
+            ),
+            Icon(
+              Icons.wash_rounded,
+              color: Colors.green,
+              size: 30.0,
+              semanticLabel: 'Wash your hands',
+            ),
+            Icon(
+              Icons.six_ft_apart_rounded,
+              color: Colors.blue,
+              size: 36.0,
+              semanticLabel: 'Socially distance',
+            ),
+          ],
         ),
-        Icon(
-          Icons.wash_rounded,
-          color: Colors.green,
-          size: 30.0,
-          semanticLabel: 'Wash your hands',
-        ),
-        Icon(
-          Icons.six_ft_apart_rounded,
-          color: Colors.blue,
-          size: 36.0,
-          semanticLabel: 'Socially distance',
-        ),
-      ],
+      ),
     );
   }
 }
