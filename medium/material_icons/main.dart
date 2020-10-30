2aadd08b1bbba8c0fe2aa57dec9d9bf90fdd548a
Jose Alba <jaguado@google.com>
2020-10-30 16:58:30 -0400

    Material icon medium dartpad demo (#75)
    * Material icon medium dartpad demo
    
    * removed unnecessary widget cast
    
    * wrapped with scaffold and center

---
+++ medium/material_icons/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Material Icon Demo',
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: const [
+            Icon(
+              Icons.masks_rounded,
+              color: Colors.pink,
+              size: 24.0,
+              semanticLabel: 'Wear a mask',
+            ),
+            Icon(
+              Icons.wash_rounded,
+              color: Colors.green,
+              size: 30.0,
+              semanticLabel: 'Wash your hands',
+            ),
+            Icon(
+              Icons.six_ft_apart_rounded,
+              color: Colors.blue,
+              size: 36.0,
+              semanticLabel: 'Socially distance',
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
